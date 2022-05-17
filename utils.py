cfb001995989989198543fefd5ce7237c99bafcb
mfrizzo <mfrizzo@ufm.edu>
2022-05-17 14:18:15 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -74,159 +74,153 @@ def decide_move(board: list, player_id: str):
         enemy = "O"
 
 # Ganar :)
-    if board [0][0] == player_id and board [0][1] == player_id and board[0,2] == "-":
+    if board [0][0] == player_id and board [0][1] == player_id and board[0][2] == "-":
         return [0,2]
 
-    if board [1][0] == player_id and board [1][1] == player_id and board[1,2] == "-":
+    if board [1][0] == player_id and board [1][1] == player_id and board[1][2] == "-":
         return [1,2]
 
-    if board [2][0] == player_id and board [2][1] == player_id and board[2,2] == "-":
+    if board [2][0] == player_id and board [2][1] == player_id and board[2][2] == "-":
         return [2,2]
     
-    if board [0][0] == player_id and board [0][1] == player_id and board[0,2] == "-":
+    if board [0][0] == player_id and board [0][1] == player_id and board[0][2] == "-":
         return [0,2]
     
-    if board [0][1] == player_id and board [0][2] == player_id and board[0,0] == "-":
+    if board [0][1] == player_id and board [0][2] == player_id and board[0][0] == "-":
         return [0,0]
     
-    if board [1][1] == player_id and board [1][2] == player_id and board[0,1] == "-":
+    if board [1][1] == player_id and board [1][2] == player_id and board[0][1] == "-":
         return [0,1]
 
-    if board [2][1] == player_id and board [2][2] == player_id and board[2,0] == "-":
+    if board [2][1] == player_id and board [2][2] == player_id and board[2][0] == "-":
         return [2,0]
     
-    if board [0][0] == player_id and board [1][0] == player_id and board[2,0] == "-":
+    if board [0][0] == player_id and board [1][0] == player_id and board[2][0] == "-":
         return [2,0]
     
-    if board [0][1] == player_id and board [1][1] == player_id and board[2,1] == "-":
+    if board [0][1] == player_id and board [1][1] == player_id and board[2][1] == "-":
         return [2,1]
     
-    if board [0][2] == player_id and board [1][2] == player_id and board[2,2] == "-":
+    if board [0][2] == player_id and board [1][2] == player_id and board[2][2] == "-":
         return [2,2]
     
-    if board [1][0] == player_id and board [2][0] == player_id and board[0,0] == "-":
+    if board [1][0] == player_id and board [2][0] == player_id and board[0][0] == "-":
         return [0,0]
 
-    if board [1][1] == player_id and board [2][1] == player_id and board[0,1] == "-":
+    if board [1][1] == player_id and board [2][1] == player_id and board[0][1] == "-":
         return [0,1]
     
-    if board [1][2] == player_id and board [2][2] == player_id and board[0,2] == "-":
+    if board [1][2] == player_id and board [2][2] == player_id and board[0][2] == "-":
         return [0,2]
     
-    if board [0][0] == player_id and board [1][1] == player_id and board[2,2] == "-":
+    if board [0][0] == player_id and board [1][1] == player_id and board[2][2] == "-":
         return [2,2]
     
-    if board [0][2] == player_id and board [1][1] == player_id and board[2,0] == "-":
+    if board [0][2] == player_id and board [1][1] == player_id and board[2][0] == "-":
         return [2,0]
     
-    if board [2][2] == player_id and board [1][1] == player_id and board[0,0] == "-":
+    if board [2][2] == player_id and board [1][1] == player_id and board[0][0] == "-":
         return [0,0]
     
-    if board [2][0] == player_id and board [1][1] == player_id and board[0,2] == "-":
+    if board [2][0] == player_id and board [1][1] == player_id and board[0][2] == "-":
         return [0,2]
 
-    if board [0][0] == player_id and board [2][0] == player_id and board[1,0] == "-":
+    if board [0][0] == player_id and board [2][0] == player_id and board[1][0] == "-":
         return [1,0]
 
-    if board [0][1] == player_id and board [2][1] == player_id and board[1,1] == "-":
+    if board [0][1] == player_id and board [2][1] == player_id and board[1][1] == "-":
         return [1,1]
     
-    if board [0][2] == player_id and board [2][2] == player_id and board[1,2] == "-":
+    if board [0][2] == player_id and board [2][2] == player_id and board[1][2] == "-":
         return [1,2]
     
-    if board [0][0] == player_id and board [0][2] == player_id and board[0,1] == "-":
+    if board [0][0] == player_id and board [0][2] == player_id and board[0][1] == "-":
         return [0,1]
     
-    if board [1][0] == player_id and board [1][2] == player_id and board[1,1] == "-":
+    if board [1][0] == player_id and board [1][2] == player_id and board[1][1] == "-":
         return [1,1]
     
-    if board [2][0] == player_id and board [2][2] == player_id and board[2,1] == "-":
+    if board [2][0] == player_id and board [2][2] == player_id and board[2][1] == "-":
         return [2,1]
     
-    if board [0][0] == player_id and board [2][2] == player_id and board[1,1] == "-":
+    if board [0][0] == player_id and board [2][2] == player_id and board[1][1] == "-":
         return [1,1]
     
-    if board [2][0] == player_id and board [0][2] == player_id and board[1,1] == "-":
+    if board [2][0] == player_id and board [0][2] == player_id and board[1][1] == "-":
         return [1,1]
 
 # Bloquear mi enemy
 
-    if board[0][0] == enemy and board [0][1] == enemy and board[0,2] == "-":
+    if board[0][0] == enemy and board [0][1] == enemy and board[0][2] == "-":
         return [0,2]
 
-    if board[1][0] == enemy and board [1][1] == enemy and board[1,2] == "-":
+    if board[1][0] == enemy and board [1][1] == enemy and board[1][2] == "-":
         return [1,2]
 
-    if board[2][0] == enemy and board [2][1] == enemy and board[2,2] == "-":
+    if board[2][0] == enemy and board [2][1] == enemy and board[2][2] == "-":
         return [2,2]
 
-    if board[1][0] == enemy and board [1][1] == enemy and board[1,2] == "-":
-        return [1,2]
-
-    if board[1][0] == enemy and board [1][1] == enemy and board[1,2] == "-":
-        return [1,2]
-
-    if board[0][1] == enemy and board [0][2] == enemy and board[0,0] == "-":
+    if board[0][1] == enemy and board [0][2] == enemy and board[0][0] == "-":
         return [0,0]
 
-    if board[1][1] == enemy and board [1][2] == enemy and board[1,0] == "-":
+    if board[1][1] == enemy and board [1][2] == enemy and board[1][0] == "-":
         return [1,0]
 
-    if board[2][1] == enemy and board [2][2] == enemy and board[2,0] == "-":
+    if board[2][1] == enemy and board [2][2] == enemy and board[2][0] == "-":
         return [2,0]
 
-    if board[0][0] == enemy and board [1][0] == enemy and board[2,0] == "-":
+    if board[0][0] == enemy and board [1][0] == enemy and board[2][0] == "-":
         return [2,0]
     
-    if board[0][1] == enemy and board [1][1] == enemy and board[2,1] == "-":
+    if board[0][1] == enemy and board [1][1] == enemy and board[2][1] == "-":
         return [2,1]
     
-    if board[0][2] == enemy and board [1][2] == enemy and board[2,2] == "-":
+    if board[0][2] == enemy and board [1][2] == enemy and board[2][2] == "-":
         return [2,2]
 
-    if board[1][0] == enemy and board [2][0] == enemy and board[0,0] == "-":
+    if board[1][0] == enemy and board [2][0] == enemy and board[0][0] == "-":
         return [0,0]
 
-    if board[1][1] == enemy and board [2][1] == enemy and board[0,1] == "-":
+    if board[1][1] == enemy and board [2][1] == enemy and board[0][1] == "-":
         return [0,1]
     
-    if board[2][2] == enemy and board [1][2] == enemy and board[0,2] == "-":
+    if board[2][2] == enemy and board [1][2] == enemy and board[0][2] == "-":
         return [0,2]
 
-    if board[0][0] == enemy and board [1][0] == enemy and board[2,0] == "-":
+    if board[0][0] == enemy and board [1][0] == enemy and board[2][0] == "-":
         return [2,0]
     
-    if board[0][0] == enemy and board [1][1] == enemy and board[2,2] == "-":
+    if board[0][0] == enemy and board [1][1] == enemy and board[2][2] == "-":
         return [2,2]
 
-    if board[0][2] == enemy and board [1][1] == enemy and board[2,0] == "-":
+    if board[0][2] == enemy and board [1][1] == enemy and board[2][0] == "-":
         return [2,0]
 
-    if board[2][0] == enemy and board [1][1] == enemy and board[0,2] == "-":
+    if board[2][0] == enemy and board [1][1] == enemy and board[0][2] == "-":
         return [0,2]
 
-    if board[2][2] == enemy and board [1][2] == enemy and board[0,0] == "-":
+    if board[2][2] == enemy and board [1][2] == enemy and board[0][0] == "-":
         return [0,0]
     
-    if board[0][0] == enemy and board [2][2] == enemy and board[1,1] == "-":
+    if board[0][0] == enemy and board [2][2] == enemy and board[1][1] == "-":
         return [1,1]
     
-    if board[0][2] == enemy and board [2][0] == enemy and board[1,1] == "-":
+    if board[0][2] == enemy and board [2][0] == enemy and board[1][1] == "-":
         return [0,0]
 
-    if board[0][0] == enemy and board [2][0] == enemy and board[1,0] == "-":
+    if board[0][0] == enemy and board [2][0] == enemy and board[1][0] == "-":
         return [1,0]
     
-    if board[0][1] == enemy and board [2][1] == enemy and board[1,1] == "-":
+    if board[0][1] == enemy and board [2][1] == enemy and board[1][1] == "-":
         return [1,1]
     
-    if board[0][2] == enemy and board [2][2] == enemy and board[1,2] == "-":
+    if board[0][2] == enemy and board [2][2] == enemy and board[1][2] == "-":
         return [1,2]
     
-    if board[1][0] == enemy and board [1][2] == enemy and board[1,1] == "-":
+    if board[1][0] == enemy and board [1][2] == enemy and board[1][1] == "-":
         return [1,1]
     
-    if board[2][0] == enemy and board [2][2] == enemy and board[2,1] == "-":
+    if board[2][0] == enemy and board [2][2] == enemy and board[2][1] == "-":
         return [2,1]
 
     row = randint(0, 2)
